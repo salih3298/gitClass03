872087ee3dbfb5ef115049a0f24350452c8e4411
salih3298 <salih3298@gmail.com>
2023-08-03 02:22:18 +0300

    the code that tells me the availibility of the product

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        boolean productAvailable = true;
+
+        if (productAvailable) {
+            System.out.print("yes");
+        } else {
+            System.out.print("no");
+        }
+    }
+}
